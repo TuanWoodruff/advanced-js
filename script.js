648c34f49f7e829da9d697ed12ab6464c56ee642
Tuan <woodrufftuan@gmail.com>
2019-04-09 21:19:04 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,90 @@
-var john = {
-    name: "John", 
-    yearOfBirth: 1990,
-    job: "teach"
+/*var Person = function(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
 };
+
+Person.prototype.calAge = function() {
+        console.log(2019 - this.yearOfBirth);
+};
+
+Person.prototype.lastName = "Smith";
+
+
+
+var john = new Person("John", 1990, "Teach");
+var jane = new Person("Jane", 1969, "Designer");
+var mark = new Person("Mark", 1930, "Retired")
+john.calAge();
+
+console.log(john.lastName, mark.lastName, jane.lastName);
+*/
+
+
+
+var Car = function(model, year, color) {
+    this.model = model;
+    this.year = year;
+    this.color = color;
+};
+
+Car.prototype.age = function() {
+    console.log(2019 - this.year);
+};
+
+var monteCarlo = new Car("Chevy", 1997, "Red");
+var cruze = new Car("Chevy", 2016, "White");
+
+cruze.age();
+monteCarlo.age();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
